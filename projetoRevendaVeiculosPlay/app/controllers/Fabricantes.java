283656c3afbbec9b5c27b04e1066b614379b00c8
leonardo.lucena.71 <leonardo.lucena.71@gmail.com>
2013-03-26 18:46:21 +0000

    Alteração do Controlador de Fabricante

--- projetoRevendaVeiculosPlay/app/controllers/Fabricantes.java
+++ projetoRevendaVeiculosPlay/app/controllers/Fabricantes.java
@@ -8,41 +8,41 @@ import play.data.Form;
 import play.mvc.Controller;
 import play.mvc.Result;
 import dao.DAOFabricante;
+import views.html.Crud.*;
 
 public class Fabricantes extends Controller {
-	
-	private static Form<Fabricante> formFabricante = new Form<Fabricante>(Fabricante.class);
-	private static Repositorio<Fabricante> repFabricante = new Repositorio<Fabricante>(new DAOFabricante());
-	
-	public static Result salvar() {
+
+	private static Form<Fabricante> formFabricante = new Form<Fabricante>(
+			Fabricante.class);
+	private static Repositorio<Fabricante> repFabricante = new Repositorio<Fabricante>(
+			new DAOFabricante());
+
+    public static Result salvar() { 
 		Form<Fabricante> novoFabricante = formFabricante.bindFromRequest();
 		Fabricante fabricante = novoFabricante.get();
-		if(fabricante.id == null){
+		if (fabricante.id == null) {
 			repFabricante.inserir(fabricante);
-		}
-		else{
+			flash("sucess", "Inserido com sucesso.");
+		} else {
 			repFabricante.atualizar(fabricante);
+			flash("sucess", "Atualizado com sucesso.");
 		}
-		
 		return redirect(routes.Fabricantes.listar());
 	}
-	
+
 	public static Result listar() {
 		List<Fabricante> lista = repFabricante.todos();
-		
-		return ok(views.html.Crud.fabricantes.render(lista, formFabricante, null));
+		return ok(fabricantes.render(lista, formFabricante, false));
 	}
-	
+
 	public static Result editar(Integer id) {
 		List<Fabricante> lista = repFabricante.todos();
 		Fabricante emEdicao = repFabricante.getPorId(id);
-		
-		return ok(views.html.Crud.fabricantes.render(lista, formFabricante.fill(emEdicao), emEdicao));
+		return ok(fabricantes.render(lista, formFabricante.fill(emEdicao), true));
 	}
-	
+
 	public static Result remover(Integer id) {
 		repFabricante.excluir(id);
-		
 		return redirect(routes.Fabricantes.listar());
 	}
 }
